ebfe0aedda8d724b34ee8ef7195f5c63ab0c917d
Hackbright Student <info@hackbrightacademy.com>
2016-01-06 11:56:31 -0800

    Great new calculator!

---
+++ arithmetic.py
@@ -0,0 +1,31 @@
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+def multiply(num1, num2):
+    result = num1 * num2
+    return result
+
+def divide(num1, num2):
+    result = num1 / num2
+    return result
+
+def square(num1):
+    """return the square of provided number"""
+    return num1 * num1
+
+def cube(num1):
+    """return the cube of the number"""
+    return num1 * num1 * num1
+
+def power(num1, num2):
+    """return result of raising num1 to the power of num2"""
+    return num1 ** num2
+
+def mod(num1, num2):
+    """returns the remainder when num1 is divided by num2"""
+    return num1 % num2
